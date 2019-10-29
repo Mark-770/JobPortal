b504860b2b9a4d73961ae4c41e3ef1d7e3b87f40
Mark <pokep123@rambler.ru>
2019-10-30 00:44:25 +0300

    Add all my files

---
+++ resources/views/company/company.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+@section('content')
+    <div class="container">
+        <h2 >Companies</h2>
+        <div class="row">
+            @foreach($companies as $company)
+
+            <div class="col-md-3 "style="margin-top: 100px;">
+                <div class="card" style="width: 18rem;">
+                    @if (empty($company->cover_photo))
+                        <img src="{{ asset('cover/tumblr-image-sizes-banner.png') }}" alt="banner" class="card-img-top"  style="width: 100%;">
+                    @else
+                        <img src="{{ asset('uploads/coverphoto') }}/{{ $company->cover_photo }}" alt="cover-photo"  class="card-img-top" style="width: 100%;">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title text-center">{{$company->cname}}</h5>
+                        <center><a href="{{ route('company.index', [$company->id, $company->slug]) }}" class="btn btn-primary">View Company</a></center>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <br><br>
+        {{$companies->links()}}
+
+    </div>
+
+@endsection
---
+++ resources/views/demo.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <h1>
+        MARK
+    </h1>
+
+@endsection
